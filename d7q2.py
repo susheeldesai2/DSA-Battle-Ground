51f7f5c03e267381b49314fa6613737ea2b309cd
susheeldesai2 <144296233+susheeldesai2@users.noreply.github.com>
2025-03-21 16:11:31 -0500

    Update d7q2.py

--- d7q2.py
+++ d7q2.py
@@ -1,3 +1,9 @@
+"""MAXIMUM GAP
+Given an integer array nums, return the maximum difference between two successive elements in its sorted form. If the array contains less than two elements, return 0.
+
+You must write an algorithm that runs in linear time and uses linear extra space.
+"""
+
 class Solution(object):
     def maximumGap(nums):
         """
